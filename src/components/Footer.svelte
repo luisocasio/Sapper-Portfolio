2b95506f5a3c07284eb4407c44431c5f48cb380c
luis ocasio <ocasio@luiss-MacBook-Pro.local>
2020-10-04 09:05:51 -0400

    implemented footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,32 +1,45 @@
 <footer class="footer">
   <div class="social-media-icons">
-    <a href="https://www.instagram.com/fullstack_og/" alt="social-media-icon">
-      <img
-        src="https://img.icons8.com/fluent/48/000000/instagram-new.png"
-        alt="instagram-logo"
-      />
-    </a>
-    <a href="https://twitter.com/FullstackOg" alt="social-media-icon">
-      <img
-        src="https://img.icons8.com/fluent/48/000000/twitter.png"
-        alt="twitter-logo"
-      />
-    </a>
-    <a href="https://www.linkedin.com/in/ocasio-perez/" alt="social-media-icon">
-      <img
-        src="https://img.icons8.com/office/48/000000/linkedin.png"
-        alt="linkedin-logo"
-      />
-    </a>
+    <div class="social-media-box">
+      <a href="https://www.instagram.com/fullstack_og/" alt="social-media-icon">
+        <img
+          src="https://img.icons8.com/fluent/48/000000/instagram-new.png"
+          alt="instagram-logo"
+        />
+      </a>
+    </div>
+
+    <div class="social-media-box">
+      <a href="https://twitter.com/FullstackOg" alt="social-media-icon">
+        <img
+          src="https://img.icons8.com/fluent/48/000000/twitter.png"
+          alt="twitter-logo"
+        />
+      </a>
+    </div>
+
+    <div class="social-media-box">
+      <a
+        href="https://www.linkedin.com/in/ocasio-perez/"
+        alt="social-media-icon"
+      >
+        <img
+          src="https://img.icons8.com/office/48/000000/linkedin.png"
+          alt="linkedin-logo"
+        />
+      </a>
+    </div>
   </div>
 
   <div class="column-wrapper">
+
     <div class="column-footer-1">
       <a href="#top">Back to top of page</a>
     </div>
 
     <div class="column-footer-1">
-      <p>email: Ocasio.dev@icloud.com</p>
+      <p>Ocasio.dev@icloud.com</p>
+      <p></p>
     </div>
 
     <div class="column-footer-1">
@@ -39,26 +52,39 @@
 <style>
   .column-wrapper {
     width: 100%;
+    height: 50%;
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
+    margin-top:2.5rem ;
   }
   .column-footer-1 {
+    width: 12.5%;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: space-center;
   }
   .social-media-icons {
     width: 100%;
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
+  }
+  .social-media-box{
+    width: 12.5%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
   .footer {
     width: 100%;
-    height: 15rem;
+    height: 7.5rem;
     bottom: 0;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
+    margin-top: 10rem;
+    padding: 1rem;
+    background-color: #21367f;
   }
 </style>
