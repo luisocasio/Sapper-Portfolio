8912b9880a06f4799cbe28b13cb2c465ae42c240
luis ocasio <ocasio@luiss-MacBook-Pro.local>
2020-10-04 11:23:10 -0400

    about layout complete

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,85 @@
   <title>About</title>
 </svelte:head>
 
-<section>
-  <h1>WELCOME TO MY PORTFOLIO.</h1>
+<section class="about-wrapper">
+  <div class="section-1">
+    <div class="content-section">
+      <h1 class="welcome-title">Welcome,</h1>
+      <p class="welcome-paragraph">
+        I am a full stack web developer from Pennsylvania.
+      </p>
+      <img src="assets/photos/littleguy.svg" alt="a child reading" />
+    </div>
+
+    <section class="text-section">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis maiores
+        quo voluptatum natus, ab nostrum quas quisquam? Minus dolor tempore
+        omnis alias expedita. Nesciunt nisi nostrum illum tenetur dignissimos
+        blanditiis!
+      </p>
+    </section>
+  </div>
+
+  <div class="section-2">
+    <section class="text-section">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis maiores
+        quo voluptatum natus, ab nostrum quas quisquam? Minus dolor tempore
+        omnis alias expedita. Nesciunt nisi nostrum illum tenetur dignissimos
+        blanditiis!
+      </p>
+    </section>
+
+    <div class="image-section">
+      <img src="assets/photos/kai.svg" alt="german shepherd puppy" />
+    </div>
+  </div>
 </section>
+
+<style>
+  .about-wrapper {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  img {
+    width: 50%;
+  }
+  .section-1 {
+    display: flex;
+    justify-content: space-between;
+  }
+  .welcome-title {
+    margin: 1.5rem;
+	font-weight: 400;
+  }
+  .welcome-paragraph{
+	  margin: 1rem;
+  }
+  .content-section {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .text-section {
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+  }
+  .section-2 {
+    display: flex;
+  }
+  .image-section {
+    width: 50%;
+    display: flex;
+    justify-content: center;
+  }
+  .text-section {
+    width: 50%;
+  }
+</style>
