3cee0950ffb90070e4856a70acf80cfbba7a90b9
luis ocasio <ocasio@luiss-MacBook-Pro.local>
2020-10-04 22:10:52 -0400

    retrieve data from prismic

--- prismic-config.js
+++ prismic-config.js
@@ -1,12 +1,29 @@
 import Prismic from "prismic-javascript";
 
-export const apiEndpoint: {};
+const apiEndpoint = "https://fullstack-blog.cdn.prismic.io/api/v2";
 
-export const Client = Prismic.client(apiEndpoint);
+export const Client = Prismic.getApi(apiEndpoint)
+  .then(function (api) {
+    return api.query(""); // An empty query will return all the documents
+  })
+  .then(
+    function (response) {
+      console.log("Documents: ", response.results);
+    },
+    function (err) {
+      console.log("Something went wrong: ", err);
+    }
+  );
 
+// Link Resolver
 export const linkResolver = (doc) => {
-  if (doc.type === "blog_post") return `/blog/${doc.uid}`;
-  if (doc.type === "blog_home") return "/blog";
-  if (doc.type === "about") return "/about";
+  // Define the url depending on the document type
+  if (doc.type === "page") {
+    return "/page/" + doc.uid;
+  } else if (doc.type === "blog_post") {
+    return "/blog/" + doc.uid;
+  }
+
+  // Default to homepage
   return "/";
 };
