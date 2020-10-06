34d6c11759ef0f4ee08af6cbea5e829cc367062a
luis ocasio <ocasio@luiss-MacBook-Pro.local>
2020-10-06 12:06:29 -0400

    content from prismic rendering

---
+++ src/routes/blog/[uid].svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  import { onMount } from "svelte";
+  import PrismicDOM from "prismic-dom";
+  import { Client, linkResolver } from "../../../prismic-config.js";
+
+  let document = null;
+
+  export async function preload({ params, query }) {
+    const uid = params.uid;
+    document = await Client.getByUID("blog_post", uid);
+
+    if (document) {
+      const title = PrismicDOM.RichText.asText(document.data.title);
+      return { document, title };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
+</script>
