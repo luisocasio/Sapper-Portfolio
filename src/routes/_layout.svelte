afbb1de8fed3562d5948395fd6b7dbc2f60f94f6
luis ocasio <ocasio@luiss-MacBook-Pro.local>
2020-10-18 00:08:21 -0400

    add segment to nav

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,13 @@
 <script>
   import Nav from "../components/Nav.svelte";
   import Footer from "../components/Footer.svelte";
+
+  export let segment
 </script>
 
 <style></style>
 
-<Nav />
+<Nav {segment}/>
 <main>
   <slot> </slot>
 </main>
